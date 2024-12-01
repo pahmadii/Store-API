4f3db409761d2d1ef1aa9d1e546a0e67ddcbf61b
pahmadii <fairyahmadi71@gmail.com>
2024-12-01 11:25:03 +0330

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const express=require('express');
 const app=express();
+const connectDB=require('./db/connect');
 
 const notFoundMiddleware=require('./middleware/not-found');
 const errordMiddleware=require('./middleware/error-handler');
@@ -17,6 +18,8 @@ app.get('/',(req,res)=>{
 res.send('<h1>Store API </h1><a href="/api/vi/products">prouducts route</a>')
 })
 
+
+
 //prouduct route
 
 app.use(notFoundMiddleware);
@@ -27,7 +30,7 @@ const port=process.env.PORT || 3000;
 
 const start=async()=>{
     try {
-        //db
+        await connectDB(process.env.MONGO_URI);
         app.listen(port,console.log(`listening on port ${port}....`))
     } catch (error) {
         console.log(error);
