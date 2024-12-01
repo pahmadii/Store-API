d9384ab756287cec15a2e83ff2c9a291d7152060
pahmadii <fairyahmadi71@gmail.com>
2024-12-01 16:03:08 +0330

    Update populate.js

--- populate.js
+++ populate.js
@@ -10,11 +10,15 @@ const jsonProducts=require('./products.json');
 const start=async()=>{
     try {
         await connectDB(process.env.MONGO_URI);
-       console.log('succese...');
+        await Product.deleteMany();
+        await Product.create(jsonProducts);
+        console.log('succese...');
+        process.exit(0);
        
     } catch (error) {
         console.log(error);
-        
+        process.exit(1);
+
     }
 }
 
