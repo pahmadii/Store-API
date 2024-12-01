3cda2fdfa050292e88f4496cf77ac91959d8c74b
pahmadii <fairyahmadi71@gmail.com>
2024-12-01 11:16:24 +0330

    Update app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+//async errors
+
+
+const express=require('express');
+const app=express();
+
+const notFoundMiddleware=require('./middleware/not-found');
+const errordMiddleware=require('./middleware/error-handler');
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get('/',(req,res)=>{
+res.send('<h1>Store API </h1><a href="/api/vi/products">prouducts route</a>')
+})
+
+//prouduct route
+
+app.use(notFoundMiddleware);
+app.use(errordMiddleware);
+
+
+const port=process.env.PORT || 3000;
+
+const start=async()=>{
+    try {
+        //db
+        app.listen(port,console.log(`listening on port ${port}....`))
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+start();
