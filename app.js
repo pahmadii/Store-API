4c4f5914e3d4846ae87c4202cdef6125c6579e90
pahmadii <fairyahmadi71@gmail.com>
2024-12-01 12:15:08 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 require('dotenv').config();
-
-//async errors
-
+require('express-async-errors');
 
 const express=require('express');
 const app=express();
-const connectDB=require('./db/connect');
-const productsRouter=require('./routes/products');
+
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 
-const notFoundMiddleware=require('./middleware/not-found');
-const errordMiddleware=require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
 
 //middleware
 app.use(express.json());
-
 //routes
+
 app.get('/',(req,res)=>{
-res.send('<h1>Store API </h1><a href="/api/vi/products">prouducts route</a>')
-})
+res.send('<h1>Store API </h1><a href="/api/v1/products">prouducts route</a>')
+});
 
 
-app.use('/api/v1/products',productsRouter);
+app.use('/api/v1/products', productsRouter);
+
 
 //prouduct route
 
 app.use(notFoundMiddleware);
-app.use(errordMiddleware);
+app.use(errorMiddleware);
 
 
 const port=process.env.PORT || 3000;
