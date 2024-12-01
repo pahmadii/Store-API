8ead5e888cd68dc7e713a04a2dc12b25283d5f30
pahmadii <fairyahmadi71@gmail.com>
2024-12-01 16:35:36 +0330

    test on postman /controller\products.js added page

--- controller/products.js
+++ controller/products.js
@@ -1,15 +1,21 @@
+const { query } = require('express');
 const Product=require('../models/product');
 
 
 const getAllProductsStatic=async (req,res)=>{
 
-    const products=await Product.find({featured:true});
+    const products=await Product.find({
+        page:'2',
+    });
     res.status(200).json({products, nbHits: products.length});
 };
 
 
 const getAllProducts=async (req,res)=>{
-    res.status(200).json({msg:'products route'})
+
+   const products=await Product.find(req.query);
+    
+    res.status(200).json({products, nbHits: products.length})
 };
 
 
