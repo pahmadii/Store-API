8e30338f5624ce8855496a7537c18bc4e28fe906
pahmadii <fairyahmadi71@gmail.com>
2024-12-01 17:38:43 +0330

    update controller\products.js / added sort

--- controller/products.js
+++ controller/products.js
@@ -4,7 +4,7 @@ const Product=require('../models/product');
 
 const getAllProductsStatic=async (req,res)=>{
 
-    const products=await Product.find({})
+    const products=await Product.find({}).sort('-name price')
     res.status(200).json({products, nbHits: products.length});
 };
 
