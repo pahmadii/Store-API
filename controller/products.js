8ea1b755f4173b8540187918d18631bf7cbf0dfd
pahmadii <fairyahmadi71@gmail.com>
2024-12-01 12:34:30 +0330

    Update controller\products.js / throw err

--- controller/products.js
+++ controller/products.js
@@ -1,6 +1,7 @@
 
 
 const getAllProductsStatic=async (req,res)=>{
+    throw new Error('testing async err');
     res.status(200).json({msg:'products testing route'})
 };
 const getAllProducts=async (req,res)=>{
