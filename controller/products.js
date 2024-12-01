12e2f0f9cb8117d26a7ccb2a7626333c7730c263
pahmadii <fairyahmadi71@gmail.com>
2024-12-01 16:18:17 +0330

    Upadte controller\products.js

--- controller/products.js
+++ controller/products.js
@@ -3,8 +3,8 @@ const Product=require('../models/product');
 
 const getAllProductsStatic=async (req,res)=>{
 
-    const products=await Product.find({});
-    res.status(200).json({products});
+    const products=await Product.find({featured:true});
+    res.status(200).json({products, nbHits: products.length});
 };
 
 
