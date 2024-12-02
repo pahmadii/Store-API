2c5991fcfc9d7b3085ad2bbdaef6ab66971267ee
pahmadii <fairyahmadi71@gmail.com>
2024-12-02 11:18:51 +0330

    edit controller\products.js /query

--- controller/products.js
+++ controller/products.js
@@ -3,11 +3,8 @@ const Product=require('../models/product');
 
 const getAllProductsStatic=async (req,res)=>{
 
-    const products=await Product.find({})
-    .sort('name')
-    .select('name price')
-    .limit(10)
-    .skip(5);
+    const products=await Product.find({}).sort('name').select('name price');
+   
     res.status(200).json({products, nbHits: products.length});
 };
 
@@ -51,7 +48,7 @@ if(name){
     }
 
 const page=Number(req.query.page) || 1;
-const limit=Number(req.q.limit) || 10;
+const limit=Number(req.query.limit) || 10;
 const skip=(page -1)*limit;
 
 result=result.skip(skip).limit(limit);
