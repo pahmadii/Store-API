43e9751c4e67b87f46776b65220225b036fcf8e6
pahmadii <fairyahmadi71@gmail.com>
2024-12-02 12:03:58 +0330

    Update  controller\products.js / added numericFilters & operatorMap

--- controller/products.js
+++ controller/products.js
@@ -3,7 +3,7 @@ const Product=require('../models/product');
 
 const getAllProductsStatic=async (req,res)=>{
 
-    const products=await Product.find({}).sort('name').select('name price');
+    const products=await Product.find({price:{$gt:30}}).sort('price').select('name price');
    
     res.status(200).json({products, nbHits: products.length});
 };
@@ -11,7 +11,7 @@ const getAllProductsStatic=async (req,res)=>{
 
 const getAllProducts=async (req,res)=>{
 
-    const{featured,company,name,sort,fields}=req.query;
+    const{featured,company,name,sort,fields,numericFilters}=req.query;
     const queryObject={}
 
     if(featured){
@@ -26,7 +26,33 @@ if(name){
     queryObject.name={$regex:name,$options:'i'}; 
 }
 
-    //console.log(queryObject);
+if(numericFilters){
+    const operatorMap={
+    '>': '$gt',
+    '>=': '$gte',
+    '=': '$eq',
+    '<': '$lt',
+    '<=': '$lte',
+    }
+    const regEX=/\b(<|>|>=|=|<|<=)\b/g;
+    let filters=numericFilters.replace(
+        regEX,
+        (match)=>`-${operatorMap[match]}-`
+    );
+    const options=['price','rating'];
+    filters=filters.split(',').forEach((item) => {
+     const[field,operator,value]=item.split('-');
+        if(options.includes(field)){
+        queryObject[field]={[operator]:Number(value)};
+
+        }
+    
+    
+    });
+
+    
+}
+    console.log(queryObject);
 
     let result=Product.find(queryObject);
 
